3e2e682f9be073bde7c418bb025218e8cf556fb6
Sergio Madrigal <se.rgiomadrigal@gmail.com>
2022-01-15 19:30:55 +0100

    component: footer simple version design completed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,19 +1,31 @@
-import Image from 'next/image';
-import styles from '../styles/Footer.module.css';
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+import { BsTwitter, BsTwitch, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <span>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
+    <footer className={styles.container}>
+      <div className={styles.left}>
+          <div className={styles.copyImg}>
+              <Image src="/img/mdgDevLogo.png" alt="logo" width={90} height={90} />
+          </div>
+        <div className={styles.copy}>© 2022 MDGDev Solutions</div>
+      </div>
+      <div className={styles.center}>
+        <div className={styles.socialItem}>
+          <BsTwitter size={30} />
+        </div>
+        <div className={styles.socialItem}>
+          <BsTwitch size={30} />
+        </div>
+        <div className={styles.socialItem}>
+          <BsInstagram size={30} />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.menuHeader}></div>
+        
+      </div>
     </footer>
   );
 };
