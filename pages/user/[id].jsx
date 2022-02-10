218600aa9d58931257eef87f8a0800d05712d021
Sergio Madrigal <se.rgiomadrigal@gmail.com>
2022-02-10 21:25:59 +0100

    new: user dashboard start

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -3,10 +3,28 @@ import Image from "next/image";
 
 const User = () => {
   return (
-      <div className={styles.container}>
-          User
+    <div className={styles.container}>
+      <div className={styles.userPanelHeader}>
+        <div className={styles.userProfile}>
+          <div className={styles.userImage}>
+            <Image
+              src="/img/avatar.svg"
+              alt="avatar"
+              width={150}
+              height={150}
+            />
+          </div>
+          <div className={styles.userInfo}>
+              <h2 className={styles.userName}>mdgdev</h2>
+              <h3 className={styles.userAdditionalInfo}>Last login: Sat 23</h3>
+              <h3 className={styles.userAdditionalInfo}>Last login: Sat 23</h3>
+
+          </div>
+        </div>
+        <div className={styles.userSummary}>User Summary</div>
       </div>
-  )
+    </div>
+  );
 };
 
 export default User;
