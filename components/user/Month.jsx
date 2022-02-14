36eb9efe5c35fe6dd63b44c6b7164b0d7b6ac660
Sergio Madrigal <sergiomadrigal@gmail.com>
2022-02-14 16:13:58 +0100

    feat: Month and MonthList created

---
+++ components/user/Month.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/Month.module.css";
+
+const Month = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>Month</div>
+    </div>
+  );
+};
+
+export default Month;
---
+++ components/user/MonthList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MonthList = () => {
+  return <div></div>;
+};
+
+export default MonthList;
